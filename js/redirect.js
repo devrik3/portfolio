83df8464fcc8d4144186f7632d95e50b56daeb12
devrik3 <93089528+devrik3@users.noreply.github.com>
2021-11-13 03:30:39 -0300

    Add files via upload

---
+++ js/redirect.js
@@ -0,0 +1,9 @@
+function wallet(){
+	window.location.replace('https://devrik3.github.io/BrWallet/');
+}
+function nubanco(){
+	window.location.replace('https://devrik3.github.io/nubanco/');
+}
+function github(){
+	window.location.replace('https://github.com/devrik3/');
+}
